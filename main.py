e90c095244de7a51a0d5331bf42a965456f9e302
student <Kozin_dn78@student.itstep.org>
2023-05-23 19:14:02 +0300

    використання методу інфо

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ try:
     print(10/0)
 except Exception:
     logging.exception('помилоочкаааааааа')
+#факторіал числа
+
+def factorial(n):
+    logging.info(f'розпочато обчислення факторіалу числа {n}')
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    logging.info(f'закінчено обчислення факторіалу числа {n} Результат виконання {result}')
+    return result
+logging.basicConfig(level= logging.INFO)
+factorial(5)
