bb8a3adf6874ba7940e8c35968779b65c9fd3ebc
student <Kozin_dn78@student.itstep.org>
2023-05-23 20:30:51 +0300

    ex2

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-import  logging
-
-class Calculator:
-    def __init__(self):
-        self.logger = logging.getLogger("Calculator")
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(logging.StreamHandler())
-    def add(self, a, b):
-        result = a + b
-        self.logger.info(f" {a} + {b} = {result}")
-        return result
-        def sub(a, b):
-            result = a - b
-        self.logger.info(f" {a} - {b} = {result}")
-        return result
-
-        def mult(a, b):
-            self.logger.info(f" {a} * {b} = {result}")
-        return result
-        def div(self , b):
-            result = a/b
-            if b == 0:
-                try:
-                    a / b
-                except ValueError:
-            else:
-                raise ValueError('POMULKAAA')
-                return add, sub, mult, div
-                add, sub, mult, div = calc()
-calc1 = Calculator()
-calc1.add(6,7)
-calc1.div(3, 1)
-calc1.mult(4,6)
-calc1.sub(14,4)
+#2
+import logging
+def write_file(fill_pass,data):
+    try:
+        with open(fill_path, 'w') as file:
+            file.write(data)
+    except Exception as e:
+        logging.error(f'сталася помилка під час зачитування з файлу {fill-path}: {str(e)}
+logging.basicConfig(level=logging.INFO)
+write_file('output.txt', input('dвведіть що треба ввести в файл:'))
