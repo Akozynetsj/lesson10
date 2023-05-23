1a431d7874339bb9762deb9bc425bc5c123f9f9b
student <Kozin_dn78@student.itstep.org>
2023-05-23 18:49:53 +0300

    організація повідомлень

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import logging #standartna biblioteka dlyz lgyvannya perebigy prigram
-
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(massage)s')
+logging.debug('debug')
+logging.info('info')
+logging.error('error')
+logging.warning('warning')
+logging.critical('critical')
