c5a797712d92be877963a373e2a7ada8f51a8728
student <Kozin_dn78@student.itstep.org>
2023-05-23 20:46:20 +0300

    ex3

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#2
+import random
 import logging
-def write_file(fill_pass,data):
+def rand_file(file_path,num ):
     try:
         with open(fill_path, 'w') as file:
-            file.write(data)
-    except Exception as e:
-        logging.error(f'сталася помилка під час зачитування з файлу {fill-path}: {str(e)}
-logging.basicConfig(level=logging.INFO)
-write_file('output.txt', input('dвведіть що треба ввести в файл:'))
+            for i in range(num):
+                data = random.randint(1, 100)
+                 file.write(str(data)+ '\n')
+                #далі допишіть логуваняя
+    except:
+logging.basicConfig(level= logging.INFO)
+def rand_file('input_random.txt',int(input(('кількість згенерованих чисел: ')))
