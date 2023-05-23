ff465c3fb9317ee509ac24b1c7d5d52ae2e64604
student <Kozin_dn78@student.itstep.org>
2023-05-23 20:08:58 +0300

    ex1

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-import logging #стандартна бібліотека для логування перебігу програми
-logging.basicConfig(level=logging.DEBUG,
-                    filename= "logs.log",
-                    filemode= 'w',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-logging.debug("debug")
-logging.info("info")
-logging.error("погано написав програму error")
-logging.warning("warning")
-logging.critical("critical")
-try:
-    print(10/0)
-except Exception:
-    logging.exception('помилоочкаааааааа')
-#факторіал числа
+import  logging
 
-def factorial(n):
-    logging.info(f'розпочато обчислення факторіалу числа {n}')
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    logging.info(f'закінчено обчислення факторіалу числа {n} Результат виконання {result}')
-    return result
-logging.basicConfig(level= logging.INFO)
-factorial(5)
+class Calculator:
+    def __init__(self):
+        self.logger = logging.getLogger("Calculator")
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(logging.StreamHandler())
+    def add(self, a, b):
+        result = a + b
+        self.logger.info(f" {a} + {b} = {result}")
+        return result
+        def sub(a, b):
+            result = a - b
+        self.logger.info(f" {a} - {b} = {result}")
+        return result
+
+        def mult(a, b):
+            self.logger.info(f" {a} * {b} = {result}")
+        return result
+        def div(self , b):
+            result = a/b
+            if b == 0:
+                try:
+                    a / b
+                except ValueError:
+            else:
+                raise ValueError('POMULKAAA')
+                return add, sub, mult, div
+                add, sub, mult, div = calc()
+calc1 = Calculator()
+calc1.add(6,7)
+calc1.div(3, 1)
+calc1.mult(4,6)
+calc1.sub(14,4)
